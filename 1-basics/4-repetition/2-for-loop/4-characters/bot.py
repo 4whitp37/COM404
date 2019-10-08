9cde973175a3e98a0503505140ba42d1173f5228
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-08 11:12:43 +0000

    Added code to demonstrate the use of for loops in string indexing.

---
+++ 1-basics/4-repetition/2-for-loop/4-characters/bot.py
@@ -0,0 +1,9 @@
+#for loop - character
+number = 0
+
+print("What strange markings do you see?")
+markings = str(input())
+
+for position in range(0, len(markings), 1):
+    print("index " + str(number) + ": " +  markings[position])
+    number = number +1
