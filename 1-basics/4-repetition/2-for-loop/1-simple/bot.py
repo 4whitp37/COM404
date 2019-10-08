d48d137f687f706df1cc1133671aef9dd6086952
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-08 10:14:28 +0000

    added code to do simple for loop

---
+++ 1-basics/4-repetition/2-for-loop/1-simple/bot.py
@@ -0,0 +1,13 @@
+#for loop simple
+count = 0
+
+print("How many mountains should I display?")
+mountains = int(input())
+
+for count in range(0,mountains,1):
+    print("            __")
+    print("           /  \_")  
+    print("         /^    \ ")
+    print("        /  ^    \_")
+    print("       /  ^      \_")
+    
