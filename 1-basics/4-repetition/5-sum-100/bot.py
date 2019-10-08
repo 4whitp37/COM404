53b1431b4da119abf5d6ec2541d415487ff9b14d
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-08 09:35:24 +0000

    Added code to demonstrate the use of a while loop with len function.

---
+++ 1-basics/4-repetition/5-sum-100/bot.py
@@ -0,0 +1,11 @@
+count = 1
+total = 0
+
+print("Calculating the sum of the first 100 numbers...")
+
+while count<=100:
+    total = total+count
+    count +=1
+
+
+print("...Done! The answer is " + str(total))
