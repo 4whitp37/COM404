f619f0e18b273db2cc8247d1e65a8c15126713e2
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-08 09:19:54 +0000

    Added code to demonstrate the use of a while loop with len function.

---
+++ 1-basics/4-repetition/1-while-loop/4-len/bot.py
@@ -0,0 +1,10 @@
+#while loop based on characters in a phrase
+word = 0
+print("Please enter a phrase:")
+character = input()
+charlen = len(character)
+ 
+while (word < charlen):
+    print("Bop")
+    word = word+1
+
