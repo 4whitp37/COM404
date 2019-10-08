16cac26875b9c6a5fde14671edb2be63f2bbf05c
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-08 11:18:47 +0000

    Started activity 5

--- 1-basics/4-repetition/2-for-loop/4-characters/bot.py
+++ 1-basics/4-repetition/2-for-loop/4-characters/bot.py
@@ -4,6 +4,6 @@ number = 0
 print("What strange markings do you see?")
 markings = str(input())
 
-for position in range(0, len(markings), 1):
+for position in range(0, len(markings), -1):
     print("index " + str(number) + ": " +  markings[position])
     number = number +1
---
+++ 1-basics/4-repetition/2-for-loop/5-reverse/bot.py
@@ -0,0 +1,4 @@
+#for loop - text in reverse
+number = 0
+
+print("What phrase do you see?")
