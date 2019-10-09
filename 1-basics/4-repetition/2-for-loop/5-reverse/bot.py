58a8f5dc3ffc2dc8372283ea7893b6b49790ac20
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-09 17:14:39 +0000

    amended code

--- 1-basics/4-repetition/2-for-loop/5-reverse/bot.py
+++ 1-basics/4-repetition/2-for-loop/5-reverse/bot.py
@@ -1,4 +1,10 @@
 #for loop - text in reverse
-number = 0
+word = ""
 
 print("What phrase do you see?")
+phrase =str(input())
+
+for position in range(0,len(phrase), -1):
+    print("The phrase is: " + phrase[position])
+    #number = number +1
+
