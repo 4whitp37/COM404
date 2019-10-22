45cbbac225f152d85019a99b999d5cf2c1e80459
4whitp37 <4whitp37a@solent.ac.uk>
2019-10-22 08:09:27 +0000

    TCA - Input and Output

--- 1-basics/5-functions/8-function-calls/bot.py
+++ 1-basics/5-functions/8-function-calls/bot.py
@@ -1,6 +1,7 @@
 #function-calls
 
 word="Hello"
+timestorepeat = 5
 
 def displaybox(word):
     print("------")
@@ -23,5 +24,14 @@ def displaymirrored(word):
     for position in range(len(word)-1,-1, -1):
         print(word[position], end='')
     print()
+    print()
         
 displaymirrored(word)
+
+def displayrepeat(word, timestorepeat):
+    for count in range(0,timestorepeat,1):
+        print(word.lower()),(word.upper())
+        
+
+displayrepeat(word, timestorepeat) 
+
---
+++ 1-basics/TCA/Q1/bot.py
@@ -0,0 +1,4 @@
+#Input and Output
+print("What happens when the last petal falls?")
+answer = input()
+print("My dear Bella when the last petal falls " + answer)
